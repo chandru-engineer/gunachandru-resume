5c80b01826054dc9d1ebd2043be872f08443f21f
Chandru <tochandru.engineer@gmail.com>
2022-10-17 20:03:02 +0530

    Debug = True push

--- digitresume/settings.py
+++ digitresume/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4z4dl@fa($xoqy%1feh(y5#789e2ze6hs3$yq)i&34@3fic@vo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['gunachandru-resume.herokuapp.com', '127.0.0.1']
